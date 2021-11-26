0b060918a308f48f33b15ff8aee7c8e30c3a2af0
vinicioslop <vinicioslop7@hotmail.com>
2021-11-26 16:59:36 -0300

    Mexe na aparência do projeto

--- Projeto2021_2/Program.cs
+++ Projeto2021_2/Program.cs
@@ -6,26 +6,31 @@ namespace Projeto2021_2
     {
         static void Main(string[] args)
         {
-            int opcao = 1;
+            int opcao = -1;
 
             do
             {
-                Console.WriteLine("Bem-vindo ao Projeto 2021/2!\n");
+                Console.Clear();
 
-                Console.WriteLine("Escolha uma das opções abaixo para");
-                Console.WriteLine("realização de cálculos matemáricos.\n");
-                Console.WriteLine("1 - SOMA");
-                Console.WriteLine("2 - SUBTRAÇÃO");
-                Console.WriteLine("3 - MULTIPLICAÇÃO");
-                Console.WriteLine("4 - DIVISÃO");
-                Console.WriteLine("5 - EXPONENCIAÇÃO");
-                Console.WriteLine("6 - RADICIAÇÃO");
-                Console.WriteLine("7 - FATORIAL");
-                Console.WriteLine("0 - SAIR");
+                Console.WriteLine("+**************************************************+");
+                Console.WriteLine("|         Bem-vindo ao Projeto 2021/2!             |");
+                Console.WriteLine("+==================================================+");
+                Console.WriteLine("| Escolha uma das opções abaixo para realização de |");
+                Console.WriteLine("| cálculos matemáticos.                            |");
+                Console.WriteLine("+==================================================+");
+                Console.WriteLine("| 1 - SOMA                                         |");
+                Console.WriteLine("| 2 - SUBTRAÇÃO                                    |");
+                Console.WriteLine("| 3 - MULTIPLICAÇÃO                                |");
+                Console.WriteLine("| 4 - DIVISÃO                                      |");
+                Console.WriteLine("| 5 - EXPONENCIAÇÃO                                |");
+                Console.WriteLine("| 6 - RADICIAÇÃO                                   |");
+                Console.WriteLine("| 7 - FATORIAL                                     |");
+                Console.WriteLine("| 0 - SAIR                                         |");
+                Console.WriteLine("+--------------------------------------------------+");
 
                 try
                 {
-                    Console.Write("\nOpção desejada: ");
+                    Console.Write("\n Opção desejada: ");
                     opcao = Convert.ToInt32(Console.ReadLine());
                 }
                 catch (Exception ex)
@@ -39,8 +44,8 @@ namespace Projeto2021_2
         }
         static void TrataErro(Exception ex)
         {
-            Console.WriteLine("\nOcorreu algum erro durante a execução! Tente novamente.");
-            Console.WriteLine("Erro: " + ex.Message);
+            Console.WriteLine("\n Ocorreu algum erro durante a execução! Tente novamente.");
+            Console.WriteLine(" Erro: " + ex.Message);
             EsperaTecla();
         }
         static void DirecionaOpcao(int opcao)
@@ -69,31 +74,35 @@ namespace Projeto2021_2
                     Fatorial();
                     break;
                 case 0:
-                    Console.WriteLine("\nSaindo...");
+                    Console.WriteLine("\n Saindo...");
                     break;
                 default:
-                    Console.WriteLine("Opção inválida!");
+                    Console.WriteLine(" Opção inválida!");
                     EsperaTecla();
                     break;
             }
         }
         static void EsperaTecla()
         {
-            Console.WriteLine("\nPressione qualquer tecla para continuar...");
+            Console.WriteLine("\n Pressione qualquer tecla para continuar...");
             Console.ReadKey();
         }
         static void Somar()
         {
+            Console.Clear();
+
+            Console.WriteLine("+**************************************************+");
+            Console.WriteLine("|              INTERFACE DE SOMA                   |");
+            Console.WriteLine("+--------------------------------------------------+\n");
+
             double valor01 = 0, valor02 = 0;
-            
-            Console.WriteLine("INTERFACE DE SOMA\n");
 
             try
             {
-                Console.Write("Informe o primeiro valor: ");
+                Console.Write(" Informe o primeiro valor: ");
                 valor01 = Convert.ToDouble(Console.ReadLine());
 
-                Console.Write("Informe o segundo valor.: ");
+                Console.Write(" Informe o segundo valor.: ");
                 valor02 = Convert.ToDouble(Console.ReadLine());
             }
             catch (Exception ex)
@@ -104,23 +113,26 @@ namespace Projeto2021_2
 
             double sum = valor01 + valor02;
 
-            Console.Write($"{valor01} + {valor02} = {sum}");
+            Console.Write($" {valor01} + {valor02} = {sum}");
 
             EsperaTecla();
         }
         static void Subtrair()
         {
+            Console.Clear();
+
+            Console.WriteLine("+**************************************************+");
+            Console.WriteLine("|            INTERFACE DE SUBTRAÇÃO                |");
+            Console.WriteLine("+--------------------------------------------------+\n");
+
             double valor01 = 0, valor02 = 0;
-            
-            
-            Console.WriteLine("INTERFACE DE SUBTRAÇÃO\n");
 
             try
             {
-                Console.Write("Informe o primeiro valor: ");
+                Console.Write(" Informe o primeiro valor: ");
                 valor01 = Convert.ToDouble(Console.ReadLine());
 
-                Console.Write("Informe o segundo valor.: ");
+                Console.Write(" Informe o segundo valor.: ");
                 valor02 = Convert.ToDouble(Console.ReadLine());
             }
             catch (Exception ex)
@@ -131,22 +143,26 @@ namespace Projeto2021_2
 
             double sub = valor01 - valor02;
 
-            Console.Write($"{valor01} - {valor02} = {sub}");
+            Console.Write($" {valor01} - {valor02} = {sub}");
 
             EsperaTecla();
         }
         static void Multiplicar()
         {
+            Console.Clear();
+
+            Console.WriteLine("+**************************************************+");
+            Console.WriteLine("|          INTERFACE DE MULTIPLICAÇÃO              |");
+            Console.WriteLine("+--------------------------------------------------+\n");
+
             double valor01 = 0, valor02 = 0;
-            
-            Console.WriteLine("INTERFACE DE MULTIPLICAÇÃO\n");
 
             try
             {
-                Console.Write("Informe o primeiro valor: ");
+                Console.Write(" Informe o primeiro valor: ");
                 valor01 = Convert.ToDouble(Console.ReadLine());
 
-                Console.Write("Informe o segundo valor.: ");
+                Console.Write(" Informe o segundo valor.: ");
                 valor02 = Convert.ToDouble(Console.ReadLine());
             }
             catch (Exception ex)
@@ -157,22 +173,26 @@ namespace Projeto2021_2
 
             double mult = valor01 * valor02;
 
-            Console.Write($"{valor01} * {valor02} = {mult}");
+            Console.Write($" {valor01} * {valor02} = {mult}");
 
             EsperaTecla();
         }
         static void Dividir()
         {
+            Console.Clear();
+
+            Console.WriteLine("+**************************************************+");
+            Console.WriteLine("|             INTERFACE DE DIVISÃO                 |");
+            Console.WriteLine("+--------------------------------------------------+\n");
+
             double dividendo = 0, divisor = 0;
-            
-            Console.WriteLine("INTERFACE DE DIVISÃO\n");
 
             try
             {
-                Console.Write("Informe o dividendo: ");
+                Console.Write(" Informe o dividendo: ");
                 dividendo = Convert.ToDouble(Console.ReadLine());
 
-                Console.Write("Informe o divisor..: ");
+                Console.Write(" Informe o divisor..: ");
                 divisor = Convert.ToDouble(Console.ReadLine());
             }
             catch (Exception ex)
@@ -183,22 +203,26 @@ namespace Projeto2021_2
 
             double div = dividendo / divisor;
 
-            Console.Write($"{dividendo} / {divisor} = {div:N3}");
+            Console.Write($" {dividendo} / {divisor} = {div:N3}");
 
             EsperaTecla();
         }
         static void Exponenciacao()
         {
+            Console.Clear();
+
+            Console.WriteLine("+**************************************************+");
+            Console.WriteLine("|           INTERFACE DE EXPONENCIAÇÃO             |");
+            Console.WriteLine("+--------------------------------------------------+\n");
+
             double valorBase = 0, valorExpoente = 0;
-            
-            Console.WriteLine("INTERFACE DE EXPONENCIAÇÃO\n");
 
             try
             {
-                Console.Write("Informe o valor da base....: ");
+                Console.Write(" Informe o valor da base....: ");
                 valorBase = Convert.ToDouble(Console.ReadLine());
 
-                Console.Write("Informe o valor do expoente: ");
+                Console.Write(" Informe o valor do expoente: ");
                 valorExpoente = Convert.ToDouble(Console.ReadLine());
             }
             catch (Exception ex)
@@ -209,22 +233,27 @@ namespace Projeto2021_2
 
             double pot = Math.Pow(valorBase, valorExpoente);
 
-            Console.Write($"{valorBase} ^ {valorExpoente} = {pot}");
+            Console.Write($" {valorBase} ^ {valorExpoente} = {pot}");
 
             EsperaTecla();
         }
         static void Radiciacao()
         {
+            Console.Clear();
+
+            Console.WriteLine("+**************************************************+");
+            Console.WriteLine("|            INTERFACE DE RADICIAÇÃO               |");
+            Console.WriteLine("+--------------------------------------------------+\n");
+
+
             double radicando = 0, indice = 0;
-            
-            Console.WriteLine("INTERFACE DE RADICIAÇÃO\n");
 
             try
             {
-                Console.Write("Informe o radicando: ");
+                Console.Write(" Informe o radicando: ");
                 radicando = Convert.ToDouble(Console.ReadLine());
 
-                Console.Write("Informe o índice...: ");
+                Console.Write(" Informe o índice...: ");
                 indice = Convert.ToDouble(Console.ReadLine());
             }
             catch (Exception ex)
@@ -235,20 +264,24 @@ namespace Projeto2021_2
 
             double resultado = Math.Pow(radicando, 1 / indice);
 
-            Console.Write($"O resultado é {resultado:N3}");
+            Console.Write($" O resultado é {resultado:N3}");
 
             EsperaTecla();
         }
 
         static void Fatorial()
         {
+            Console.Clear();
+
+            Console.WriteLine("+**************************************************+");
+            Console.WriteLine("|            INTERFACE DE FATORIAL                 |");
+            Console.WriteLine("+--------------------------------------------------+\n");
+
             int fat = 1, entrada = 0;
-            
-            Console.WriteLine("INTERFACE DE FATORIAL\n");
 
             try
             {
-                Console.Write("Informe um valor inteiro: ");
+                Console.Write(" Informe um valor inteiro: ");
                 entrada = Convert.ToInt32(Console.ReadLine());
 
                 if (entrada < 0)
@@ -264,7 +297,7 @@ namespace Projeto2021_2
 
             if (entrada == 0)
             {
-                Console.WriteLine($"\n{entrada}! = {fat + 1}");
+                Console.WriteLine($"\n {entrada}! = {fat + 1}");
                 return;
             }
 
@@ -273,7 +306,7 @@ namespace Projeto2021_2
                 fat *= i;
             }
 
-            Console.WriteLine($"\n{entrada}! = {fat}");
+            Console.WriteLine($"\n {entrada}! = {fat}");
 
             EsperaTecla();
         }
